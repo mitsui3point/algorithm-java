46e4f965e02387ba78dda967073467b69e2a64c2
mitsui3point <rladbsgh0331@gmail.com>
2023-05-17 23:31:23 +0900

    feat($a_string): add - 3. 문장 속 단어

---
+++ src/main/java/org/example/a_string/Ex03ChooseLongestWord.java
@@ -0,0 +1,59 @@
+package org.example.a_string;
+
+import java.util.Scanner;
+
+/**
+ * 3. 문장 속 단어
+ * 설명
+ * 한 개의 문장이 주어지면 그 문장 속에서 가장 긴 단어를 출력하는 프로그램을 작성하세요.
+ * 문장속의 각 단어는 공백으로 구분됩니다.
+ *
+ * 입력
+ * 첫 줄에 길이가 100을 넘지 않는 한 개의 문장이 주어집니다. 문장은 영어 알파벳으로만 구성되어 있습니다.
+ *
+ * 출력
+ * 첫 줄에 가장 긴 단어를 출력한다. 가장 길이가 긴 단어가 여러개일 경우 문장속에서 가장 앞쪽에 위치한 단어를 답으로 합니다.
+ *
+ * it is time to study => study
+ */
+public class Ex03ChooseLongestWord {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            Ex03ChooseLongestWord main = new Ex03ChooseLongestWord();
+            String result = main.solution(input);
+            System.out.println(result);
+            return ;
+        }
+
+        private String solution(String input) {
+            String result = "";
+            int wordLength = 0;
+            for (String s : input.split(" ")) {
+                if (wordLength < s.length()) {
+                    wordLength = s.length();
+                    result = s;
+                }
+            }
+            return result;
+        }
+/*        private String solution(String input) {
+            String result = "";
+            int wordLength = 0;
+            int spaceIndex = input.indexOf(" ") + 1;;
+            while (spaceIndex != 0) {
+                String compareTarget = input.substring(0, spaceIndex).trim();
+                input = input.substring(spaceIndex);
+                if (wordLength < compareTarget.length()) {
+                    wordLength = compareTarget.length();
+                    result = compareTarget;
+                }
+                spaceIndex = input.indexOf(" ") + 1;
+            }
+            if (wordLength < input.trim().length()) {
+                wordLength = input.trim().length();
+                result = input.trim();
+            }
+            return result;
+        }*/
+}
